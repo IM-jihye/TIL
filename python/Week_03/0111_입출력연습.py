8e538be68ac39d2057621d1aba30260b7a1d701e
IM-jihye <jihye1662@naver.com>
2023-01-11 17:16:45 +0900

    python

--- python/Week_03/0111_입출력연습.py
+++ python/Week_03/0111_입출력연습.py
@@ -7,20 +7,20 @@ print(numbers)
 # 문제 2
 # 공백으로 구분된 문자열
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-# string = input().split()
-# print(string)
+string = input().split()
+print(string)
 
 # 문제 3
 # 테스트 케이스 수와 입력 줄 수가 주어지는 입력
 # 첫 줄에 테스트 케이스 수가 주어집니다.
 # 각 테스트 케이스의 첫 번째 줄에 입력 줄 수가 주어집니다.
-# T = int(input()) # 테스트 케이스 수
+T = int(input()) # 테스트 케이스 수
 
-# for t in range(1, T+1):
-#     N = int(input()) # 입력 줄 수
+for t in range(1, T+1):
+    N = int(input()) # 입력 줄 수
     
-#     for i in range(1,1+N):
-#         print(i)
+    for i in range(1,1+N):
+        print(i)
 
 # 문제 4
 # 테스트 케이스 수와 입력 줄 수가 주어지는 입력
@@ -28,15 +28,15 @@ print(numbers)
 # 각 테스트 케이스의 첫 번째 줄에 입력 줄 수가 주어집니다.
 # T = int(input()) # 테스트 케이스 수
 
-# for t in range(1, T+1):
-#     N = int(input()) # 입력 줄 수
+for t in range(1, T+1):
+    N = int(input()) # 입력 줄 수
     
-#     for i in range(1,1+N):
-#         print(i,i)
+    for i in range(1,1+N):
+        print(i,i)
 
 # 문제 5
 # 테스트 케이스 수와 입력 줄 수가 주어지는 입력
-# 첫 줄에 테스트 케이스 수가 주어집니다.
+# 첫 줄에 테스트 케이스 수가 주어집니
 # 각 테스트 케이스의 첫 번째 줄에 입력 줄 수가 주어집니다.
 # 각 문장에 포함된 단어를 공백을 기준으로 출력하세요.
 # input.txt 파일을 생성하고,입력을 작성해주세요.
